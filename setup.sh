977f0f53ddf8ae5247cee3f7b8b8b1fc0ec84973
e.nikitin <e.nikitin@sirena-travel.ru>
2021-07-16 00:13:51 +0300

    docker
    for docker

--- setup.sh
+++ setup.sh
@@ -49,4 +49,4 @@ chown -R mastmetric:mastmetric /usr/share/alert_bot/
 logrotate -vdf *
 mkdir -p /dev/cache && chmod 777 /dev/cache && mount -t tmpfs -o size=100M tmpfs /dev/cache && mkdir -p /dev/cache/1 && mkdir -p /dev/cache/2 && su mastmetric -c '/usr/share/alert_bot/abot1.sh' -s /bin/bash &
 
-[ "$1" -eq "1" ] && su mastmetric -c '/usr/share/alert_bot/trbot.sh' -s /bin/bash 
+[ "$1" -eq "1" ] && su mastmetric -c '/usr/share/alert_bot/trbot.sh 2' -s /bin/bash 
